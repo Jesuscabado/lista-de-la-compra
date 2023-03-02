6ddc79f5834758895bc5aa6089106f39f32a2aff
Jesuscabado <jesuscabado@hotmail.com>
2023-03-02 16:04:57 +0100

    se puede borrar el post

--- blog.js
+++ blog.js
@@ -7,11 +7,28 @@ function createPost(event) {
   let contenido = document.getElementById("contenido").value.trim();
   let h3 = document.createElement("h3");
   let p = document.createElement("p");
+  let icono = document.createElement("i");
+  icono.classList.add("fa","fa-trash");
   let article = document.createElement("article");
   h3.innerText = titulo;
   p.innerText = contenido;
   article.appendChild(h3);
   article.appendChild(p);
+  article.appendChild(icono);
   nuevosBlog.appendChild(article);
+  nuevosBlog.insertBefore(article, nuevosBlog.children[1]);
+  icono.addEventListener("click",deletePost);
+
   document.getElementById("blogForm").reset();
+}
+
+function deletePost(event) {
+    let element = event.target;
+    let parent = element.parentElement;
+    parent.remove();
+    let text = parent.getElementsByTagName("h3")[0].innerText;
+    if(confirm("¿Deseas borrar este post? \n" + text)){
+   
+        parent.remove();
+    }
 }
